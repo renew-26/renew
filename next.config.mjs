2c04124f6ce9142e733036dd4388c521f3f49df4
김동현 <doyn0822@hiconsy.com>
2024-11-07 11:15:23 +0900

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true
+    },
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/renew' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/renew' : '',
+};
 
 export default nextConfig;
