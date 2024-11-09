57d6d9b4cc4f4c73ae9e4507a5aef9be5730ec15
renew-26 <hyunnnn0822@gmail.com>
2024-11-09 15:57:27 +0900

    기본 컴포넌트 작업 중 (#7)
    Co-authored-by: 김동현 <doyn0822@hiconsy.com>

---
+++ app/_components/docs/AppBox.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import classNames from '@utils/classNames';
+
+interface AppBoxProps {
+  children: React.ReactNode;
+  bg?: 'gray' | 'blue' | 'green' | 'orange';
+  outline?: boolean;
+  className?: string | string[];
+}
+
+const AppBox: React.FC<AppBoxProps> = ({ children, bg = 'gray', outline = false, className }) => {
+  const PREFIX = 'app-box';
+
+  return (
+    <div
+      className={classNames(
+        PREFIX,
+        bg && `${PREFIX}--${bg}`,
+        outline ? `${PREFIX}--outline` : '',
+        Array.isArray(className) ? className.join(' ') : className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default AppBox;
