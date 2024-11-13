26c1dc808778d81135ac03990bc05499e48b5766
renew-26 <hyunnnn0822@gmail.com>
2024-11-13 12:49:47 +0900

    Footer 스타일 수정 (#9)
    Co-authored-by: 김동현 <doyn0822@hiconsy.com>

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import { Heading } from 'hyun-design-system';
+import { Container, Text } from 'hyun-design-system';
 
 export default function Footer() {
   const PREFIX = 'footer';
 
   return (
     <footer className={PREFIX}>
-      <Heading>Footer</Heading>
+      <Container>
+        <div className="footer-text">
+          <Text color="tertiary">© ReNew Company</Text>
+        </div>
+        <div className="footer-link">
+          <div className='link-item link-naver'>N</div>
+          <div className='link-item link-email'>E</div>
+          <div className='link-item link-phone'>P</div>
+        </div>
+      </Container>
     </footer>
   );
 }
