273a08bebb5c5a8cb67ca4056532240e6df27fd8
renew-26 <hyunnnn0822@gmail.com>
2024-11-13 18:12:39 +0900

    Footer 스타일 수정, SNS 아이콘 추가 (#11)
    Co-authored-by: 김동현 <doyn0822@hiconsy.com>

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Container, Text, Stack } from 'hyun-design-system';
+import Image from 'next/image';
 
 export default function Footer() {
   const PREFIX = 'footer';
@@ -8,7 +9,7 @@ export default function Footer() {
   return (
     <footer className={PREFIX}>
       <Container>
-        <Stack direction="col" className="footer-text">
+        <div className="footer-text">
           <Stack className="footer-info" gap={4}>
             <Text size="sm">㈜ReNew 대표: 김동현</Text>
             <Text size="sm">사업자등록번호: *****-******</Text>
@@ -16,13 +17,32 @@ export default function Footer() {
             <Text size="sm">전화: 010-****-****</Text>
             <Text size="sm">이메일: k010111323@gmail.com</Text>
           </Stack>
-          <Text color="tertiary">©COPYRIGHT (주)ReNew. All Rights Reserved.</Text>
-        </Stack>
+        </div>
 
-        <div className="footer-link">
-          <div className="link-item link-naver">N</div>
-          <div className="link-item link-email">E</div>
-          <div className="link-item link-phone">P</div>
+        <div className="footer-copyright">
+          <Text color="tertiary">©COPYRIGHT (주)ReNew. All Rights Reserved.</Text>
+          <ul className="footer-link">
+            <li>
+              <a target="_blank" href="네이버블로그 주소" className="link-item link-naver">
+                <Image src={'/sns-icon/icon_naver.png'} width={30} height={30} alt="네이버 아이콘" />
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://open.kakao.com/o/{오픈채팅방ID}"
+                rel="noopener"
+                className="link-item link-kakao"
+              >
+                <Image src={'/sns-icon/icon_kakao.png'} width={30} height={30} alt="카카오톡 아이콘" />
+              </a>
+            </li>
+            <li>
+              <a href="mailto:이메일주소" className="link-item link-email">
+                <Image src={'/sns-icon/icon_email.png'} width={30} height={30} alt="이메일 아이콘" />
+              </a>
+            </li>
+          </ul>
         </div>
       </Container>
     </footer>
