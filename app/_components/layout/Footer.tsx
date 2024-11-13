82c464efde2e76a899c0011f0c34d29c9a81c7f4
renew-26 <hyunnnn0822@gmail.com>
2024-11-13 16:00:29 +0900

    Footer 스타일 수정 (#10)
    Co-authored-by: 김동현 <doyn0822@hiconsy.com>

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Text } from 'hyun-design-system';
+import { Container, Text, Stack } from 'hyun-design-system';
 
 export default function Footer() {
   const PREFIX = 'footer';
@@ -8,13 +8,21 @@ export default function Footer() {
   return (
     <footer className={PREFIX}>
       <Container>
-        <div className="footer-text">
-          <Text color="tertiary">© ReNew Company</Text>
-        </div>
+        <Stack direction="col" className="footer-text">
+          <Stack className="footer-info" gap={4}>
+            <Text size="sm">㈜ReNew 대표: 김동현</Text>
+            <Text size="sm">사업자등록번호: *****-******</Text>
+            <Text size="sm">주소: 동대문구 답십리로 **길 **</Text>
+            <Text size="sm">전화: 010-****-****</Text>
+            <Text size="sm">이메일: k010111323@gmail.com</Text>
+          </Stack>
+          <Text color="tertiary">©COPYRIGHT (주)ReNew. All Rights Reserved.</Text>
+        </Stack>
+
         <div className="footer-link">
-          <div className='link-item link-naver'>N</div>
-          <div className='link-item link-email'>E</div>
-          <div className='link-item link-phone'>P</div>
+          <div className="link-item link-naver">N</div>
+          <div className="link-item link-email">E</div>
+          <div className="link-item link-phone">P</div>
         </div>
       </Container>
     </footer>
