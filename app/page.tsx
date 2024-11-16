f20e42b5c2a6d53e8732d15a39f8ff1568f77184
hyun <hyunnnn0822@gmail.com>
2024-11-16 22:45:57 +0900

    Header 수정, route 폴더 이름 변경

--- app/page.tsx
+++ app/page.tsx
@@ -3,71 +3,75 @@
 import { Container, Heading, Text } from 'hyun-design-system';
 
 import StepTitle from '@components/StepTitle';
+import HeroSection from '@layout/section/HeroSection';
 
 const Home = () => {
   return (
-    <section className="section-hero section">
-      <Container>
-        <div className="content-wrap">
-          <StepTitle>
-            <Heading level="h2">Heading Text Heading Text</Heading>
-            <Text size="xl" color="primary">
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-              <br />
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-            </Text>
-          </StepTitle>
-        </div>
-      </Container>
-      <Container>
-        <div className="content-wrap">
-          <StepTitle>
-            <Heading level="h2">Heading Text Heading Text</Heading>
-            <Text size="xl" color="primary">
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-              <br />
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-            </Text>
-          </StepTitle>
-        </div>
-      </Container>
-      <Container>
-        <div className="content-wrap">
-          <StepTitle>
-            <Heading level="h2">Heading Text Heading Text</Heading>
-            <Text size="xl" color="primary">
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-              <br />
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-            </Text>
-          </StepTitle>
-        </div>
-      </Container>
-      <Container>
-        <div className="content-wrap">
-          <StepTitle>
-            <Heading level="h2">Heading Text Heading Text</Heading>
-            <Text size="xl" color="primary">
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-              <br />
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-            </Text>
-          </StepTitle>
-        </div>
-      </Container>
-      <Container>
-        <div className="content-wrap">
-          <StepTitle>
-            <Heading level="h2">Heading Text Heading Text</Heading>
-            <Text size="xl" color="primary">
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-              <br />
-              Normal Text Normal Text Normal Text Normal Text Normal Text
-            </Text>
-          </StepTitle>
-        </div>
-      </Container>
-    </section>
+    <>
+      <HeroSection />
+      <section className="section-hero section">
+        <Container>
+          <div className="content-wrap">
+            <StepTitle>
+              <Heading level="h2">Heading Text Heading Text</Heading>
+              <Text size="xl" color="primary">
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+                <br />
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+              </Text>
+            </StepTitle>
+          </div>
+        </Container>
+        <Container>
+          <div className="content-wrap">
+            <StepTitle>
+              <Heading level="h2">Heading Text Heading Text</Heading>
+              <Text size="xl" color="primary">
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+                <br />
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+              </Text>
+            </StepTitle>
+          </div>
+        </Container>
+        <Container>
+          <div className="content-wrap">
+            <StepTitle>
+              <Heading level="h2">Heading Text Heading Text</Heading>
+              <Text size="xl" color="primary">
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+                <br />
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+              </Text>
+            </StepTitle>
+          </div>
+        </Container>
+        <Container>
+          <div className="content-wrap">
+            <StepTitle>
+              <Heading level="h2">Heading Text Heading Text</Heading>
+              <Text size="xl" color="primary">
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+                <br />
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+              </Text>
+            </StepTitle>
+          </div>
+        </Container>
+        <Container>
+          <div className="content-wrap">
+            <StepTitle>
+              <Heading level="h2">Heading Text Heading Text</Heading>
+              <Text size="xl" color="primary">
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+                <br />
+                Normal Text Normal Text Normal Text Normal Text Normal Text
+              </Text>
+            </StepTitle>
+          </div>
+        </Container>
+      </section>
+    </>
   );
 };
 
