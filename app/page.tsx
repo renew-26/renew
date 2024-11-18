584e26cd4d0f8024ce0ec6f6b5ff80ce0d49a7f4
김동현 <doyn0822@hiconsy.com>
2024-11-18 17:21:59 +0900

    Performance Section 레이아웃 추가

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import { Container, Heading, Text } from 'hyun-design-system';
 
 import StepTitle from '@components/StepTitle';
 import HeroSection from '@layout/section/HeroSection';
+import PerformanceSection from '@layout/section/PerformanceSection';
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <PerformanceSection />
       <section className="section">
         <Container>
           <div className="content-wrap">
